688d4f3e4f328374504f01090b7c9a92974e496c
kanonren <kanonren0707@icloud.com>
2026-03-05 18:38:29 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    basic.showString("sTART")
+    basic.showString("START")
     basic.showIcon(IconNames.Heart)
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    basic.showString("Stop")
+    basic.showString("STOP")
     basic.showIcon(IconNames.No)
 })
 radio.setGroup(42)
 basic.showString("d")
 basic.forever(function () {
-	
+    let spielt_gerade = 0
+    if (spielt_gerade) {
+        radio.sendString("BEAT")
+        basic.pause(500)
+    }
 })
